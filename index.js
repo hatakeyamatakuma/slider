aebc314e02fc4072888dec2a119d6c537ebd6bba
hatakeyamatakuma <hatakuma@icloud.com>
2023-01-20 18:19:56 +0900

    sixth-commit revice-index.js

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
-//フェード処理の速さ
-const fadeSpeed = 1000;
 //imgをまず全部見えなくする
 $(".key-visual img").hide();
 //key-visualの最初の部分にactiveというクラス名を付け、それを見えるようにする。
 $(".key-visual img:first").addClass("active").show();
 
-let changeImage = function(){
+const changeImage = function(){
   //activeの付いた要素を$activeと定義
-  let $active = $(".key-visual img.active")
-  //activeの付いた要素の次の要素にimgという文字列が存在するか否か。存在する場合$nextに次のimgを代入。なければ最初のimgを代入し最初の画面に戻る。
-  let $next = $active.next("img").length?$active.next("img"):$(".key-visual img:first");
+  let active = $(".key-visual img.active")
+  //activeの付いた要素の次の要素にimgという文字列が存在するか否か。存在する場合nextに次のimgを代入。なければ最初のimgを代入し最初の画面に戻る。
+  let next = active.next("img").length?active.next("img"):$(".key-visual img:first");
 
-  $active.fadeOut(fadeSpeed).removeClass("active");
-  $next.fadeIn(fadeSpeed).addClass("active");
+  active.fadeOut().removeClass("active");
+  next.fadeIn().addClass("active");
 }
 
-let changeReverce = function(){
+const changeReverce = function(){
   //activeの付いた要素を$activeと定義
-  let $active = $(".key-visual img.active")
-  //activeの付いた要素の次の要素にimgという文字列が存在するか否か。存在する場合$nextに次のimgを代入。なければ最初のimgを代入し最初の画面に戻る。
-  let $prev = $active.prev("img").length?$active.prev("img"):$(".key-visual img:last");
+  let active = $(".key-visual img.active")
+  //activeの付いた要素の前の要素にimgという文字列が存在するか否か。存在する場合nextに次のimgを代入。なければ最初のimgを代入し最初の画面に戻る。
+  let prev = active.prev("img").length?active.prev("img"):$(".key-visual img:last");
 
-  $active.fadeOut(fadeSpeed).removeClass("active");
-  $prev.fadeIn(fadeSpeed).addClass("active");
+  active.fadeOut().removeClass("active");
+  prev.fadeIn().addClass("active");
 }
 
 $(".right-btn").on("click",function(){
-  console.log("右クリック")
   changeImage();
   count = 0;
 })
 
 $(".left-btn").on("click",function(){
-  console.log("左クリック")
   changeReverce();
   count = 0;
 })
 
 // 自動スライド
-var count = 0;
+let count = 0;
 
 setInterval(() => {
 if (count > 4) {
@@ -46,5 +42,4 @@ if (count > 4) {
   changeImage();
 }
 count++;
-console.log(count);
 }, 1000);
