807ccf3c1400b201aefb69f67ff1127161fd481f
hatakeyamatakuma <hatakuma@icloud.com>
2023-02-02 21:24:01 +0900

    seventh-commit revece-index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
+let count = 0;
 //imgをまず全部見えなくする
-$(".key-visual img").hide();
+$("#js-slide img").hide();
 //key-visualの最初の部分にactiveというクラス名を付け、それを見えるようにする。
-$(".key-visual img:first").addClass("active").show();
+$("#js-slide img:first").addClass("active").show();
 
 const changeImage = function(){
   //activeの付いた要素を$activeと定義
-  let active = $(".key-visual img.active")
+  const active = $("#js-slide img.active")
   //activeの付いた要素の次の要素にimgという文字列が存在するか否か。存在する場合nextに次のimgを代入。なければ最初のimgを代入し最初の画面に戻る。
-  let next = active.next("img").length?active.next("img"):$(".key-visual img:first");
+  const next = active.next("img").length?active.next("img"):$("#js-slide img:first");
 
-  active.fadeOut().removeClass("active");
-  next.fadeIn().addClass("active");
+  active.css('display', 'none').removeClass("active");
+  next.fadeIn(1000).addClass("active");
 }
 
 const changeReverce = function(){
   //activeの付いた要素を$activeと定義
-  let active = $(".key-visual img.active")
+  const active = $("#js-slide img.active")
   //activeの付いた要素の前の要素にimgという文字列が存在するか否か。存在する場合nextに次のimgを代入。なければ最初のimgを代入し最初の画面に戻る。
-  let prev = active.prev("img").length?active.prev("img"):$(".key-visual img:last");
+  const prev = active.prev("img").length?active.prev("img"):$("#js-slide img:last");
 
-  active.fadeOut().removeClass("active");
-  prev.fadeIn().addClass("active");
+  active.css('display', 'none').removeClass("active");
+  prev.fadeIn(1000).addClass("active");
 }
 
 $(".right-btn").on("click",function(){
@@ -34,7 +35,6 @@ $(".left-btn").on("click",function(){
 })
 
 // 自動スライド
-let count = 0;
 
 setInterval(() => {
 if (count > 4) {
